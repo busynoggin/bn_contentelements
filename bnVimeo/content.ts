a0320421caa52a4fc80eb756a9859d78330e3740
Ron Hall <ron@busynoggin.com>
2014-04-01 00:47:18 -0400

    Clean up bnVimeo and bnYoutube

--- bnVimeo/content.ts
+++ bnVimeo/content.ts
@@ -1,22 +1,182 @@
 tt_content.bnVimeo = COA
 tt_content.bnVimeo {
 
-	## Include the necessary Javascript and CSS libraries.
+	// Include the necessary Javascript and CSS libraries.
 
 	10 = INCLUDEJSLIBS
 	10.tx_bnvideo = EXT:bn_contentelements/res/video/bn_video.js
 
-	## Perhaps replace this later with SASS in the skin
-	20 = HEADERDATA
-	20 {
+	15 = HEADERDATA
+	15 {
 		value = typo3conf/ext/bn_contentelements/res/video/bn_video.css
 		wrap = <link rel="stylesheet" type="text/css" href="|" media="all">
 	}
 
+	//Reset register in case there are multiple videos on the page
+	20 = LOAD_REGISTER
+	20 {
+		bnVideoWidth >
+		bnVideoWidth >
+		bnVideoStyle >
+		bnVideoStyleFloat >
+		bnVideoFloatClass >
+		bnVideoStyleMargin >
+		tempImageHeight >
+		imageHeight >
+		aspectRatioClass >
+	}
+
+	// Set width
+	25 = LOAD_REGISTER
+	25.bnVideoWidth.data = register:containerWidth
+
+	30 = CASE
+	30 {
+		if.isTrue.data = t3datastructure : pi_flexform->appearance->maxWidth
+		key.data = t3datastructure : pi_flexform->appearance->maxWidthUnit
+		1 = LOAD_REGISTER
+		1 {
+			tempBNVideoWidth.data= t3datastructure : pi_flexform->appearance->maxWidth
+			tempBNVideoWidth.dataWrap = (|/100)*{register:containerWidth}
+			bnVideoWidth {
+				data = register : tempBNVideoWidth
+				prioriCalc = intval
+			}
+			bnVideoStyleWidth {
+				data = register:bnVideoWidth
+				wrap = width:|px;
+			}
+		}
+		default = LOAD_REGISTER
+		default {
+			bnVideoWidth.data= t3datastructure : pi_flexform->appearance->maxWidth
+			bnVideoStyleWidth {
+				data = register:bnVideoWidth
+				wrap = width:|px;
+			}
+		}
+	}
+
+	32 = CASE
+	32 {
+		if.isTrue.data = t3datastructure : pi_flexform->appearance->float
+		right = LOAD_REGISTER
+		right {
+			bnVideoStyleFloat {
+				data = t3datastructure : pi_flexform->appearance->float
+				wrap = float:|;
+			}
+			bnVideoFloatClass {
+				data = t3datastructure : pi_flexform->appearance->float
+				noTrimWrap = | ||
+			}
+		}
+		default = LOAD_REGISTER
+		default {
+			bnVideoStyleFloat {
+				data = t3datastructure : pi_flexform->appearance->float
+				wrap = float:|;
+			}
+			bnVideoFloatClass {
+				data = t3datastructure : pi_flexform->appearance->float
+				noTrimWrap = | ||
+			}
+		}
+	}
+
+	35 = LOAD_REGISTER
+	35 {
+		bnVideoStyleMargin {
+			data = t3datastructure : pi_flexform->appearance->margin
+			split {
+				token = ,
+				cObjNum = 1
+				1.current = 1
+				1.noTrimWrap = | |px|
+			}
+			wrap = margin:|;
+			if.isTrue.data = t3datastructure : pi_flexform->appearance->margin
+		}
+	}
+
+	37 = LOAD_REGISTER
+	37 {
+		tempBNVideoStyle {
+			dataWrap = {register:bnVideoStyleWidth}{register:bnVideoStyleMargin}{register:bnVideoStyleFloat}
+		}
+		bnVideoStyle {
+			// if.isTrue.data = register:tempBNVideoStyle
+			data = register:tempBNVideoStyle
+			noTrimWrap = |style="|"|
+		}
+	}
+
+	# Setup values for image sizing
+	40 = CASE
+	40 {
+		key.data = t3datastructure : pi_flexform->lightbox->imageAspectRatio
+		169 = LOAD_REGISTER
+		169 {
+			tempImageHeight.data = register : bnVideoWidth
+			tempImageHeight.wrap = (|*9)/16
+			imageHeight.data = register : tempImageHeight
+			imageHeight.prioriCalc = intval
+		}
+		43 = LOAD_REGISTER
+		43 {
+			tempImageHeight.data = register : bnVideoWidth
+			tempImageHeight.wrap = (|*3)/4
+			imageHeight.data = register : tempImageHeight
+			imageHeight.prioriCalc = intval
+		}
+		32 = LOAD_REGISTER
+		32 {
+			tempImageHeight.data = register : bnVideoWidth
+			tempImageHeight.wrap = (|*2)/3
+			imageHeight.data = register : tempImageHeight
+			imageHeight.prioriCalc = intval
+		}
+		11 = LOAD_REGISTER
+		11 {
+			tempImageHeight.data = register : bnVideoWidth
+			tempImageHeight.wrap = |/1
+			imageHeight.data = register : tempImageHeight
+			imageHeight.prioriCalc = intval
+		}
+		21 = LOAD_REGISTER
+		21 {
+			tempImageHeight.data = register : bnVideoWidth
+			tempImageHeight.wrap = |/2
+			imageHeight.data = register : tempImageHeight
+			imageHeight.prioriCalc = intval
+		}
+		31 = LOAD_REGISTER
+		31 {
+			tempImageHeight.data = register : bnVideoWidth
+			tempImageHeight.wrap = |/3
+			imageHeight.data = register : tempImageHeight
+			imageHeight.prioriCalc = intval
+		}
+		41 = LOAD_REGISTER
+		41 {
+			tempImageHeight.data = register : bnVideoWidth
+			tempImageHeight.wrap = |/4
+			imageHeight.data = register : tempImageHeight
+			imageHeight.prioriCalc = intval
+		}
+		51 = LOAD_REGISTER
+		51 {
+			tempImageHeight.data = register : bnVideoWidth
+			tempImageHeight.wrap = |/5
+			imageHeight.data = register : tempImageHeight
+			imageHeight.prioriCalc = intval
+		}
+	}
+
 	## Setup CSS class for aspect ratio
-	25 = CASE
-	25 {
-		key.data = t3datastructure : pi_flexform->aspectRatio
+	45 = CASE
+	45 {
+		key.data = t3datastructure : pi_flexform->video->aspectRatio
 		43 = LOAD_REGISTER
 		43 {
 			aspectRatioClass = 43
@@ -30,83 +190,22 @@ tt_content.bnVimeo {
 	}
 
 	## Setup autoplay parameter
-	30 = CASE
-	30 {
-		if.isFalse.data = t3datastructure : pi_flexform->lightboxDisplay
-		key.data = t3datastructure : pi_flexform->autoPlay
+	47 = CASE
+	47 {
+		if.isFalse.data = t3datastructure : pi_flexform->lightbox->lightboxDisplay
+		key.data = t3datastructure : pi_flexform->video->autoPlay
 		1 = LOAD_REGISTER
 		1.autoPlay = &amp;autoplay=1
 		default = LOAD_REGISTER
 		default.autoPlay = &amp;autoplay=0
 	}
 
-	# Setup values for image sizing
-	40 = CASE
-	40 {
-		key.data = t3datastructure : pi_flexform->imageAspectRatio
-		169 = LOAD_REGISTER
-		169 {
-			tempImageHeight.data = register : containerWidth
-			tempImageHeight.wrap = (|*9)/16
-			imageHeight.data = register : tempImageHeight
-			imageHeight.prioriCalc = intval
-		}
-		43 = LOAD_REGISTER
-		43 {
-			tempImageHeight.data = register : containerWidth
-			tempImageHeight.wrap = (|*3)/4
-			imageHeight.data = register : tempImageHeight
-			imageHeight.prioriCalc = intval
-		}
-		32 = LOAD_REGISTER
-		32 {
-			tempImageHeight.data = register : containerWidth
-			tempImageHeight.wrap = (|*2)/3
-			imageHeight.data = register : tempImageHeight
-			imageHeight.prioriCalc = intval
-		}
-		11 = LOAD_REGISTER
-		11 {
-			tempImageHeight.data = register : containerWidth
-			tempImageHeight.wrap = |/1
-			imageHeight.data = register : tempImageHeight
-			imageHeight.prioriCalc = intval
-		}
-		21 = LOAD_REGISTER
-		21 {
-			tempImageHeight.data = register : containerWidth
-			tempImageHeight.wrap = |/2
-			imageHeight.data = register : tempImageHeight
-			imageHeight.prioriCalc = intval
-		}
-		31 = LOAD_REGISTER
-		31 {
-			tempImageHeight.data = register : containerWidth
-			tempImageHeight.wrap = |/3
-			imageHeight.data = register : tempImageHeight
-			imageHeight.prioriCalc = intval
-		}
-		41 = LOAD_REGISTER
-		41 {
-			tempImageHeight.data = register : containerWidth
-			tempImageHeight.wrap = |/4
-			imageHeight.data = register : tempImageHeight
-			imageHeight.prioriCalc = intval
-		}
-		51 = LOAD_REGISTER
-		51 {
-			tempImageHeight.data = register : containerWidth
-			tempImageHeight.wrap = |/5
-			imageHeight.data = register : tempImageHeight
-			imageHeight.prioriCalc = intval
-		}
-	}
 
-	50 =< lib.stdheader
+	55 =< lib.stdheader
 
 	60 = CASE
 	60 {
-		key.data = t3datastructure : pi_flexform->lightboxDisplay
+		key.data = t3datastructure : pi_flexform->lightbox->lightboxDisplay
 
 		1 = FILES
 		1 {
@@ -120,22 +219,22 @@ tt_content.bnVimeo {
 			renderObj {
 				file {
 					import.data = file:current:originalUid // file:current:uid
-					width.dataWrap = {register:containerWidth}c
+					width.dataWrap = {register:bnVideoWidth}c
 					height {
 						dataWrap = {register:imageHeight}c
-						if.isTrue.data = t3datastructure : pi_flexform->imageAspectRatio
+						if.isTrue.data = t3datastructure : pi_flexform->lightbox->imageAspectRatio
 					}
 				}
-				altText.data = t3datastructure : pi_flexform->title
-				titleText.data = t3datastructure : pi_flexform->title
+				altText.data = t3datastructure : pi_flexform->video->title
+				titleText.data = t3datastructure : pi_flexform->video->title
 				stdWrap {
 					innerWrap = |<div class="play-button">&nbsp;</div>
-					innerWrap.if.isTrue.data = t3datastructure : pi_flexform->showPlayButton
+					innerWrap.if.isTrue.data = t3datastructure : pi_flexform->lightbox->showPlayButton
 					innerWrap2 = <div class="youtube-vimeo-wrap">|</div>
 					typolink {
-						parameter.dataWrap = http://player.vimeo.com/video/{t3datastructure : pi_flexform->videoID}?title=0&amp;byline=0&amp;portrait=0
-						ATagParams.dataWrap = class="bn-video youtube-vimeo-lightbox{register:aspectRatioClass}"
-						title.data = t3datastructure : pi_flexform->title
+						parameter.dataWrap = http://player.vimeo.com/video/{t3datastructure : pi_flexform->video->videoID}?title=0&amp;byline=0&amp;portrait=0
+						ATagParams.dataWrap = class="bn-video youtube-vimeo-lightbox{register:aspectRatioClass}{register:bnVideoFloatClass}"{register:bnVideoStyle}
+						title.data = t3datastructure : pi_flexform->video->title
 						target = _blank
 					}
 				}
@@ -143,7 +242,7 @@ tt_content.bnVimeo {
 		}
 		default = TEXT
 		default {
-			dataWrap = <div class="bn-video youtube-vimeo-inline{register:aspectRatioClass}"><div class="spacer">&nbsp;</div><iframe src="http://player.vimeo.com/video/{t3datastructure : pi_flexform->videoID}?title=0&amp;byline=0&amp;portrait=0{register:autoPlay}"  frameborder="0" webkitAllowFullScreen allowfullscreen></iframe></div><!-- end .youtube-vimeo-lightbox -->
+			dataWrap = <div class="bn-video youtube-vimeo-inline{register:aspectRatioClass}{register:bnVideoFloatClass}"{register:bnVideoStyle}><div class="spacer">&nbsp;</div><iframe src="http://player.vimeo.com/video/{t3datastructure : pi_flexform->video->videoID}?title=0&amp;byline=0&amp;portrait=0{register:autoPlay}"  frameborder="0" webkitAllowFullScreen allowfullscreen></iframe></div><!-- end .youtube-vimeo-lightbox -->
 		}
 	}
 
--- bnYoutube/content.ts
+++ bnYoutube/content.ts
@@ -1,31 +1,183 @@
-tt_content.bn_youtube = COA
-tt_content.bn_youtube {
+tt_content.bnYoutube = COA
+tt_content.bnYoutube {
 
 	## Include the necessary Javascript and CSS libraries.
 
 	10 = INCLUDEJSLIBS
-	10.fancybox = {$bnSiteConfig.commonResources.fancybox.js.path}
+	10.tx_bnvideo = EXT:bn_contentelements/res/video/bn_video.js
 
-	12 = INCLUDEJSLIBS
-	12.tx_bn_video = EXT:bn_video/res/bn_video.js
-	
+	## Perhaps replace this later with SASS in the skin
 	15 = HEADERDATA
 	15 {
-		value = {$bnSiteConfig.commonResources.fancybox.css.path}
+		value = typo3conf/ext/bn_contentelements/res/video/bn_video.css
 		wrap = <link rel="stylesheet" type="text/css" href="|" media="all">
 	}
 
-## Perhaps replace this later with SASS in the skin
-	20 = HEADERDATA
+	//Reset register in case there are multiple videos on the page
+	20 = LOAD_REGISTER
 	20 {
-		value = typo3conf/ext/bn_video/res/bn_video.css
-		wrap = <link rel="stylesheet" type="text/css" href="|" media="all">
+		bnVideoWidth >
+		bnVideoWidth >
+		bnVideoStyle >
+		bnVideoStyleFloat >
+		bnVideoFloatClass >
+		bnVideoStyleMargin >
+		tempImageHeight >
+		imageHeight >
+		aspectRatioClass >
+	}
+
+	// Set width
+	25 = LOAD_REGISTER
+	25.bnVideoWidth.data = register:containerWidth
+
+	30 = CASE
+	30 {
+		if.isTrue.data = t3datastructure : pi_flexform->appearance->maxWidth
+		key.data = t3datastructure : pi_flexform->appearance->maxWidthUnit
+		1 = LOAD_REGISTER
+		1 {
+			tempBNVideoWidth.data= t3datastructure : pi_flexform->appearance->maxWidth
+			tempBNVideoWidth.dataWrap = (|/100)*{register:containerWidth}
+			bnVideoWidth {
+				data = register : tempBNVideoWidth
+				prioriCalc = intval
+			}
+			bnVideoStyleWidth {
+				data = register:bnVideoWidth
+				wrap = width:|px;
+			}
+		}
+		default = LOAD_REGISTER
+		default {
+			bnVideoWidth.data= t3datastructure : pi_flexform->appearance->maxWidth
+			bnVideoStyleWidth {
+				data = register:bnVideoWidth
+				wrap = width:|px;
+			}
+		}
+	}
+
+	32 = CASE
+	32 {
+		if.isTrue.data = t3datastructure : pi_flexform->appearance->float
+		right = LOAD_REGISTER
+		right {
+			bnVideoStyleFloat {
+				data = t3datastructure : pi_flexform->appearance->float
+				wrap = float:|;
+			}
+			bnVideoFloatClass {
+				data = t3datastructure : pi_flexform->appearance->float
+				noTrimWrap = | ||
+			}
+		}
+		default = LOAD_REGISTER
+		default {
+			bnVideoStyleFloat {
+				data = t3datastructure : pi_flexform->appearance->float
+				wrap = float:|;
+			}
+			bnVideoFloatClass {
+				data = t3datastructure : pi_flexform->appearance->float
+				noTrimWrap = | ||
+			}
+		}
+	}
+
+	35 = LOAD_REGISTER
+	35 {
+		bnVideoStyleMargin {
+			data = t3datastructure : pi_flexform->appearance->margin
+			split {
+				token = ,
+				cObjNum = 1
+				1.current = 1
+				1.noTrimWrap = | |px|
+			}
+			wrap = margin:|;
+			if.isTrue.data = t3datastructure : pi_flexform->appearance->margin
+		}
+	}
+
+	37 = LOAD_REGISTER
+	37 {
+		tempBNVideoStyle {
+			dataWrap = {register:bnVideoStyleWidth}{register:bnVideoStyleMargin}{register:bnVideoStyleFloat}
+		}
+		bnVideoStyle {
+			// if.isTrue.data = register:tempBNVideoStyle
+			data = register:tempBNVideoStyle
+			noTrimWrap = |style="|"|
+		}
+	}
+
+	# Setup values for image sizing
+	40 = CASE
+	40 {
+		key.data = t3datastructure : pi_flexform->lightbox->imageAspectRatio
+		169 = LOAD_REGISTER
+		169 {
+			tempImageHeight.data = register : bnVideoWidth
+			tempImageHeight.wrap = (|*9)/16
+			imageHeight.data = register : tempImageHeight
+			imageHeight.prioriCalc = intval
+		}
+		43 = LOAD_REGISTER
+		43 {
+			tempImageHeight.data = register : bnVideoWidth
+			tempImageHeight.wrap = (|*3)/4
+			imageHeight.data = register : tempImageHeight
+			imageHeight.prioriCalc = intval
+		}
+		32 = LOAD_REGISTER
+		32 {
+			tempImageHeight.data = register : bnVideoWidth
+			tempImageHeight.wrap = (|*2)/3
+			imageHeight.data = register : tempImageHeight
+			imageHeight.prioriCalc = intval
+		}
+		11 = LOAD_REGISTER
+		11 {
+			tempImageHeight.data = register : bnVideoWidth
+			tempImageHeight.wrap = |/1
+			imageHeight.data = register : tempImageHeight
+			imageHeight.prioriCalc = intval
+		}
+		21 = LOAD_REGISTER
+		21 {
+			tempImageHeight.data = register : bnVideoWidth
+			tempImageHeight.wrap = |/2
+			imageHeight.data = register : tempImageHeight
+			imageHeight.prioriCalc = intval
+		}
+		31 = LOAD_REGISTER
+		31 {
+			tempImageHeight.data = register : bnVideoWidth
+			tempImageHeight.wrap = |/3
+			imageHeight.data = register : tempImageHeight
+			imageHeight.prioriCalc = intval
+		}
+		41 = LOAD_REGISTER
+		41 {
+			tempImageHeight.data = register : bnVideoWidth
+			tempImageHeight.wrap = |/4
+			imageHeight.data = register : tempImageHeight
+			imageHeight.prioriCalc = intval
+		}
+		51 = LOAD_REGISTER
+		51 {
+			tempImageHeight.data = register : bnVideoWidth
+			tempImageHeight.wrap = |/5
+			imageHeight.data = register : tempImageHeight
+			imageHeight.prioriCalc = intval
+		}
 	}
 
 	## Setup CSS class for aspect ratio
-	35 = CASE
-	35 {
-		key.data = t3datastructure : pi_flexform->aspectRatio
+	45 = CASE
+	45 {
+		key.data = t3datastructure : pi_flexform->video->aspectRatio
 		43 = LOAD_REGISTER
 		43 {
 			aspectRatioClass = 43
@@ -37,25 +189,12 @@ tt_content.bn_youtube {
 			aspectRatioClass.noTrimWrap = | aspect-ratio-||
 		}
 	}
-	
-	## Setup CSS class for lightbox fallback
-	40 = CASE
-	40 {
-		key.data = t3datastructure : pi_flexform->lightboxDisplay
-		1 = LOAD_REGISTER
-		1 {
-			lightboxFallbackClass = video-fallback
-			lightboxFallbackClass.noTrimWrap = | ||
-		}
-		default = LOAD_REGISTER
-		default.lightboxFallbackClass = 
-	}
 
 	## Setup autoplay parameter
-	45 = CASE
-	45 {
-		if.isFalse.data = t3datastructure : pi_flexform->lightboxDisplay
-		key.data = t3datastructure : pi_flexform->autoPlay
+	47 = CASE
+	47 {
+		if.isFalse.data = t3datastructure : pi_flexform->lightbox->lightboxDisplay
+		key.data = t3datastructure : pi_flexform->video->autoPlay
 		1 = LOAD_REGISTER
 		1.autoPlay = &amp;autoplay=1
 		default = LOAD_REGISTER
@@ -66,109 +205,58 @@ tt_content.bn_youtube {
 	50 = COA
 	50 {
 		if {
-			value.data = t3datastructure : pi_flexform->startPoint
+			value.data = t3datastructure : pi_flexform->video->startPoint
 			isLessThan = 0
 		}
 		10 = LOAD_REGISTER
 		10.videoStartPoint {
-			data = t3datastructure : pi_flexform->startPoint
-			if.isTrue.data = t3datastructure : pi_flexform->startPoint
+			data = t3datastructure : pi_flexform->video->startPoint
+			if.isTrue.data = t3datastructure : pi_flexform->video->startPoint
 			noTrimWrap = |&amp;start=||
 		}
 	}
-	
-	# Setup values for image sizing
-	52 = CASE
-	52 {
-		key.data = t3datastructure : pi_flexform->imageAspectRatio
-		169 = LOAD_REGISTER
-		169 {
-			tempImageHeight.data = register : containerWidth
-			tempImageHeight.wrap = (|*9)/16
-			imageHeight.data = register : tempImageHeight
-			imageHeight.prioriCalc = intval
-		}
-		43 = LOAD_REGISTER
-		43 {
-			tempImageHeight.data = register : containerWidth
-			tempImageHeight.wrap = (|*3)/4
-			imageHeight.data = register : tempImageHeight
-			imageHeight.prioriCalc = intval
-		}
-		32 = LOAD_REGISTER
-		32 {
-			tempImageHeight.data = register : containerWidth
-			tempImageHeight.wrap = (|*2)/3
-			imageHeight.data = register : tempImageHeight
-			imageHeight.prioriCalc = intval
-		}
-		11 = LOAD_REGISTER
-		11 {
-			tempImageHeight.data = register : containerWidth
-			tempImageHeight.wrap = |/1
-			imageHeight.data = register : tempImageHeight
-			imageHeight.prioriCalc = intval
-		}
-		21 = LOAD_REGISTER
-		21 {
-			tempImageHeight.data = register : containerWidth
-			tempImageHeight.wrap = |/2
-			imageHeight.data = register : tempImageHeight
-			imageHeight.prioriCalc = intval
-		}
-		31 = LOAD_REGISTER
-		31 {
-			tempImageHeight.data = register : containerWidth
-			tempImageHeight.wrap = |/3
-			imageHeight.data = register : tempImageHeight
-			imageHeight.prioriCalc = intval
-		}
-		41 = LOAD_REGISTER
-		41 {
-			tempImageHeight.data = register : containerWidth
-			tempImageHeight.wrap = |/4
-			imageHeight.data = register : tempImageHeight
-			imageHeight.prioriCalc = intval
-		}
-		51 = LOAD_REGISTER
-		51 {
-			tempImageHeight.data = register : containerWidth
-			tempImageHeight.wrap = |/5
-			imageHeight.data = register : tempImageHeight
-			imageHeight.prioriCalc = intval
-		}
-	}
 
-	55 = < lib.stdheader
-	
-	60 = COA
+
+	55 =< lib.stdheader
+
+	60 = CASE
 	60 {
-		10 = IMAGE
-		10 {
-			if.isTrue.data = t3datastructure : pi_flexform->lightboxDisplay
-			file {
-				import.dataWrap = uploads/tx_bnvideo/bn_youtube/{t3datastructure : pi_flexform->image}
-				width.dataWrap = {register:containerWidth}c
-				height {
-					dataWrap = {register:imageHeight}c
-					if.isTrue.data = t3datastructure : pi_flexform->imageAspectRatio
-				}
+		key.data = t3datastructure : pi_flexform->lightbox->lightboxDisplay
+
+		1 = FILES
+		1 {
+			references {
+				table.field = table
+				uid.field = uid
+				fieldName = flexform_bnvideo_image
 			}
-			altText.data = t3datastructure : pi_flexform->title
-			titleText.data = t3datastructure : pi_flexform->title
-			stdWrap {
-				innerWrap = |<div class="play-button">&nbsp;</div>
-				innerWrap.if.isTrue.data = t3datastructure : pi_flexform->showPlayButton
-				typolink {
-					parameter.dataWrap = http://www.youtube.com/embed/{t3datastructure : pi_flexform->videoID}?autohide=1&modestbranding=1&rel=0&showinfo=0{register:videoStartPoint}
-					ATagParams.dataWrap = class="bn-video youtube-vimeo-lightbox{register:aspectRatioClass}"
-					title.data = t3datastructure : pi_flexform->title
-					target = _blank
+
+			renderObj = IMAGE
+			renderObj {
+				file {
+					import.data = file:current:originalUid // file:current:uid
+					width.dataWrap = {register:bnVideoWidth}c
+					height {
+						dataWrap = {register:imageHeight}c
+						if.isTrue.data = t3datastructure : pi_flexform->lightbox->imageAspectRatio
+					}
+				}
+				altText.data = t3datastructure : pi_flexform->video->title
+				titleText.data = t3datastructure : pi_flexform->video->title
+				stdWrap {
+					innerWrap = |<div class="play-button">&nbsp;</div>
+					innerWrap.if.isTrue.data = t3datastructure : pi_flexform->lightbox->showPlayButton
+					typolink {
+						parameter.dataWrap = http://www.youtube.com/embed/{t3datastructure : pi_flexform->video->videoID}?autohide=1&modestbranding=1&rel=0&showinfo=0{register:videoStartPoint}
+						ATagParams.dataWrap = class="bn-video youtube-vimeo-lightbox{register:aspectRatioClass}{register:bnVideoFloatClass}"{register:bnVideoStyle}
+						title.data = t3datastructure : pi_flexform->video->title
+						target = _blank
+					}
 				}
 			}
 		}
-		20 = TEXT
-		20.dataWrap = <div class="bn-video youtube-vimeo-inline{register:aspectRatioClass}{register:lightboxFallbackClass}"><div class="spacer">&nbsp;</div><iframe src="http://www.youtube.com/embed/{t3datastructure : pi_flexform->videoID}?autohide=1&modestbranding=1&rel=0&showinfo=0{register:autoPlay}{register:videoStartPoint}" frameborder="0" webkitAllowFullScreen allowfullscreen></iframe></div><!-- end .youtube-vimeo-lightbox -->
+		default = TEXT
+		default.dataWrap = <div class="bn-video youtube-vimeo-inline{register:aspectRatioClass}{register:bnVideoFloatClass}"{register:bnVideoStyle}><div class="spacer">&nbsp;</div><iframe src="http://www.youtube.com/embed/{t3datastructure : pi_flexform->video->videoID}?autohide=1&modestbranding=1&rel=0&showinfo=0{register:autoPlay}{register:videoStartPoint}" frameborder="0" webkitAllowFullScreen allowfullscreen></iframe></div><!-- end .youtube-vimeo-lightbox -->
 	}
 
 	#unset the startPoint, autoPlay and lightboxFallBackClass in case other videos are on the page
@@ -176,6 +264,5 @@ tt_content.bn_youtube {
 	90 {
 		videoStartPoint =
 		autoPlay =
-		lightboxFallBackClass =
 	}
 }
